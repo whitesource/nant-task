68d78b5f507dfe1a8a254fa84001370980d31c65
Tom Shapira <tom.shapira@whitesourcesoftware.com>
2014-04-14 12:59:19 +0300

    part of previous commit

---
+++ whitesource-nant/NAntTask/CheckPolicies.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Util;
+
+namespace Whitesource.NAnt.Tasks
+{
+    
+    /**
+     * Nested element in the White Source NAnt task, which checks for policies and generates a report.
+     * 
+     * @author tom.shapira
+     */
+    [ElementName("checkpolicies")]
+    public class CheckPolicies : DataTypeBase
+    {
+        /**
+	     * Output directory for White Source generated report file.
+	     */
+        private String _reportDir;
+
+        [TaskAttribute("reportdir", Required = true)]
+        [StringValidator(AllowEmpty = false)]
+        public String ReportDir
+        {
+            get { return _reportDir; }
+            set { _reportDir = value; }
+        }
+
+        /**
+         * Whether or not to fail the build if policy rejects a library.
+         */
+        private bool _failOnRejection;
+
+        [TaskAttribute("failonrejection", Required = false)]
+        [StringValidator(AllowEmpty = false)]
+        public bool FailOnRejection
+        {
+            get { return _failOnRejection; }
+            set { _failOnRejection = value; }
+        }
+
+        /* --- Constructors --- */
+
+        public CheckPolicies()
+        {
+            _failOnRejection = true;
+        }
+
+    }
+}
